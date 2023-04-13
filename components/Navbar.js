4be064b79b15d0cbaa5ad04ba865101b65e52bbd
Maxime Bignolet <maxime.bignolet@gmail.com>
2023-04-13 10:51:02 +0200

    navbar

---
+++ components/Navbar.js
@@ -0,0 +1,44 @@
+import {
+  faFlaskVial,
+  faHome,
+  faListCheck,
+  faUsers,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="nav">
+      <ul className="nav-elements">
+        <li className="home">
+          <Link href="/">
+            <FontAwesomeIcon icon={faHome} className="icon" />
+            <p>Dashboard</p>
+          </Link>
+        </li>
+        <li>
+          <Link href="/clients">
+            <FontAwesomeIcon icon={faUsers} className="icon" />
+            <p>Clients</p>
+          </Link>
+        </li>
+        <li>
+          <Link href="/tests">
+            <FontAwesomeIcon icon={faFlaskVial} className="icon" />
+            <p>Tests</p>
+          </Link>
+        </li>
+        <li>
+          <Link href="/process">
+            <FontAwesomeIcon icon={faListCheck} className="icon" />
+            <p>Process</p>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
