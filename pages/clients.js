5ab9a68d3b43125bcca97f0bdc18d3d2a55df19a
Maxime Bignolet <maxime.bignolet@gmail.com>
2023-04-14 16:25:56 +0200

    build

--- pages/clients.js
+++ pages/clients.js
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
 import ContentClientsPage from "@/components/Clients/ContentClientsPage";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Clients = () => {
   return (
     <>
       <h1 className="client-title">Clients</h1>
-      <ContentClientsPage />
+      <AnimatePresence>
+        <motion.div
+          initial={{ opacity: 0, x: 30 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: 30 }}
+          transition={{ type: "spring", stiffness: 70 }}
+        >
+          <ContentClientsPage />
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 };
